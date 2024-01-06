e67510574a1af3c6acd558d5d74adbf145e38764
Harvey <harveyorencio@gmail.com>
2024-01-06 12:04:40 +0800

    fix head html

--- resources/views/layout/html-head.blade.php
+++ resources/views/layout/html-head.blade.php
@@ -2,11 +2,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Equali | Login </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&family=Poppins:wght@100;300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     {{-- <script src="https://unpkg.com/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
@@ -15,6 +21,22 @@
     <!--Responsive Extension Datatables CSS-->
     {{-- <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet"> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>{{-- pdf --}}
-    @vite('resources/css/app.css')
+
     <title>{{ $pageTitle ?? 'Default Title' }}</title>
+
+    <script>
+        tailwind.config = {
+            theme: {
+                fontFamily: {
+                    open: '"Open Sans"',
+                    poppins: "'Poppins', sans-serif",
+                    raleway: "'Raleway', sans-serif",
+                },
+                extend: {},
+            }
+        }
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
