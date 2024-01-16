0f7cbd85b192e72bba3a9001346d90ffdbafb8a1
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2024-01-17 01:31:24 +0800

    test ui auth

--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -18,6 +18,24 @@
             <h1 class="text-center text-4xl font-bold py-8 ">SIGN UP</h1>
             {{-- <pre class="hidden">{{ var_dump(session()->all()) }}</pre> --}}
             <div class="flex justify-center">
+                <form class="max-w-sm mx-auto">
+                    <label for="email-address-icon" class="block mb-2 text-sm font-medium text-gray-900 ">Your
+                        Email</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 20 16">
+                                <path
+                                    d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                <path
+                                    d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                            </svg>
+                        </div>
+                        <input type="text" id="email-address-icon"
+                            class="bg-transparent border-b border-gray-300 text-gray-900 text-sm rounded-lg  block w-full ps-10 p-2.5"
+                            placeholder="name@flowbite.com">
+                    </div>
+                </form>
                 <form action="{{ route('store.account') }}" method="POST" class="text-lg font-semibold text-center">
                     @csrf
                     <div class="pb-5 relative">
@@ -115,56 +133,3 @@
 </body>
 
 </html>
-
-<section class="bg-gray-50 ">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
-            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                alt="logo">
-            Flowbite
-        </a>
-        <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                    Sign in to your account
-                </h1>
-                <form class="space-y-4 md:space-y-6" action="#">
-                    <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your
-                            email</label>
-                        <input type="email" name="email" id="email"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                            placeholder="name@company.com" required="">
-                    </div>
-                    <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                            required="">
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" aria-describedby="remember" type="checkbox"
-                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
-                                    required="">
-                            </div>
-                            <div class="ml-3 text-sm">
-                                <label for="remember" class="text-gray-500 ">Remember me</label>
-                            </div>
-                        </div>
-                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline">Forgot
-                            password?</a>
-                    </div>
-                    <button type="submit"
-                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign
-                        in</button>
-                    <p class="text-sm font-light text-gray-500 ">
-                        Don’t have an account yet? <a href="#"
-                            class="font-medium text-primary-600 hover:underline">Sign up</a>
-                    </p>
-                </form>
-            </div>
-        </div>
-    </div>
-</section>
